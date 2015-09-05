214d0b31009a7394584226a0c1884c9876758436
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2015-09-05 16:02:28 +0200

    src

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello World 2");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World 2!");
+            attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
