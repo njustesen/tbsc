72c1327ddd4627e558efa41e71ef0f8c6cd65a0f
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2015-09-08 19:16:51 +0200

    syso

--- src/main/java/tbsc/server/main/Main.java
+++ src/main/java/tbsc/server/main/Main.java
@@ -5,7 +5,9 @@ import tbsc.server.rest.RestApp;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		System.out.println("SERVER RUNNING ---------------");
+		
 		DB.connect();
 		
 		new RestApp();
