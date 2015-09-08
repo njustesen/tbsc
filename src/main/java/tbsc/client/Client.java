fcdb19a5728836b84bd73654139ac2e5080c3427
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2015-09-08 19:14:27 +0200

    syso

--- src/main/java/tbsc/client/Client.java
+++ src/main/java/tbsc/client/Client.java
@@ -7,6 +7,8 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Client running ------------------");
+		
 		ServerConnection con = new ServerConnection("https://tbsc-dev.herokuapp.com");
 		
 		Session session = con.createSession("niller", "12345678");
